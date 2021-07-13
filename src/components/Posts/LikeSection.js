9b429f002c51d34b5de28c4eff963f5aae564c1e
sb7297 <sbaireddy7297@gmail.com>
2021-07-13 13:18:04 -0400

    LikeSection: indicate liking & prevent >1 like

--- src/components/Posts/LikeSection.js
+++ src/components/Posts/LikeSection.js
@@ -2,13 +2,27 @@
 // Use a piece of data coming in through props to display the correct number of likes.
 // You will also add an onClick handler that utilizes `likePost` to increase the count of likes.
 // (As a stretch goal, you might want to prevent your user from "liking" the same post more than once.)
-import React from 'react';
+import React, { useState } from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faComment, faHeart } from '@fortawesome/free-regular-svg-icons';
 
 const LikeSection = props => {
   // 🔥 Make sure the parent of LikeSection is passing the right props!
   const { likePost, numberOfLikes } = props;
+  const [liked, setLiked] = useState(false);
+
+  const clickHandler = () => {
+    if (!liked) {
+      setLiked(true);
+      likePost();
+    }
+  };
+
+  const likeButtonStyler = () => {
+    let ret = {cursor: 'pointer'};
+    if (liked) ret['color'] = 'crimson';
+    return ret;
+  };
 
   return (
     <div>
@@ -16,7 +30,7 @@ const LikeSection = props => {
         className='like-section'
         key='likes-icons-container'
       >
-        <div className='like-section-wrapper' onClick={likePost}>
+        <div className='like-section-wrapper' onClick={clickHandler} style={likeButtonStyler()}>
           <FontAwesomeIcon icon={faHeart} />
         </div>
         <div className='like-section-wrapper'>
