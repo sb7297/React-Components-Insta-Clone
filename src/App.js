1fad73c500b4ba92e095f06e7bbd1e8fc3650d88
sb7297 <sbaireddy7297@gmail.com>
2021-07-13 12:52:30 -0400

    App: likePost callback

--- src/App.js
+++ src/App.js
@@ -17,6 +17,7 @@ const App = () => {
   // Create a state called `posts` to hold the array of post objects, **initializing to dummyData**.
   // This state is the source of truth for the data inside the app. You won't be needing dummyData anymore.
   // To make the search bar work (which is stretch) we'd need another state to hold the search term.
+  const [posts, setPosts] = useState(dummyData);
 
   const likePost = postId => {
     /*
@@ -30,6 +31,13 @@ const App = () => {
         - if the `id` of the post matches `postId`, return a new post object with the desired values (use the spread operator).
         - otherwise just return the post object unchanged.
      */
+    setPosts(posts.map(post => {
+      if (post.id === postId) {
+        return { ...post, likes: post.likes+1 };
+      } else {
+        return post;
+      }
+    }));
   };
 
   return (
