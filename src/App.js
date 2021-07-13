61ddfc05832327c66bed0f828d5721153da07788
sb7297 <sbaireddy7297@gmail.com>
2021-07-13 12:45:42 -0400

    App: imports

--- src/App.js
+++ src/App.js
@@ -5,9 +5,12 @@
 */
 
 // Import the state hook
-import React from 'react';
+import React, { useState } from 'react';
 // Import the Posts (plural!) and SearchBar components, since they are used inside App component
+import Posts from './components/Posts/Posts.js';
+import SearchBar from './components/SearchBar/SearchBar.js';
 // Import the dummyData
+import dummyData from './dummy-data.js';
 import './App.css';
 
 const App = () => {
